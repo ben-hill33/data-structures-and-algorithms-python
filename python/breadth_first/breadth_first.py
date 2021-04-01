62a8f5bdd937aa8ee0b701e05a54bd5a78f43543
BEN <benjamin.hill28@gmail.com>
2021-03-31 20:36:30 -0700

    added tests

---
+++ python/breadth_first/breadth_first.py
@@ -0,0 +1,23 @@
+from data_structures.stacks_and_queues.stacks_and_queues import Queue
+from graph.graph import Graph
+
+class BF_Graph(Graph):
+
+  def breadth_first(self, vertex):
+    node = []
+    breadth = Queue()
+
+    breadth.enqueue(vertex)
+    visited = set()
+
+    while not breadth.is_empty():
+      front = breadth.dequeue()
+      node.append(front.value)
+
+      for neighbor in self.get_neighbor(front):
+        if neighbor.vertex.value not in visited:
+          visited.add(neighbor.vertex.value)
+          breadth.enqueue(neighbor.vertex)
+    return node
+
+
