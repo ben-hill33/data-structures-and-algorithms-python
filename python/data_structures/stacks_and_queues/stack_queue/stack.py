da60523b6876b745225e96588b2afc2643bc9826
BEN <benjamin.hill28@gmail.com>
2021-05-02 11:57:05 -0700

    added stack implementation

--- python/data_structures/stacks_and_queues/stack_queue/stack.py
+++ python/data_structures/stacks_and_queues/stack_queue/stack.py
@@ -1,13 +1,61 @@
-# create a node class with two properties, value for the Node and pointer to next node
-class Node:
-  # the node's default value needs to be None
-  def __init__(self, value, node=None):
-    self.value = value
-    self.node = node
+from node import Node
 
-# create a stack class with a top property, it creates an empty stack when instantiated
-# Define a method called isEmpty that takes no argument, and returns a boolean indicating whether or not the stack is empty.
-# should be aware of a default empty value assigned to top when stack is created
-# define a method called push / which takes any value as an argument and adds a new node with that value to the top of the stack with O(1) time performance
-# Define a method called pop that does not take any argument, removes the node from the top of the stack, and returns the node’s value. Should raise exception when called on empty stack
-# Define a method called peek that does not take an argument and returns the value of the node located on top of the stack, without removing it from the stack. Should raise exception when called on empty stack
+class Stack:
+  def __init__(self, limit=1000):
+    self.top_item = None
+    self.size = 0
+    self.limit = limit
+  
+  def push(self, value):
+    item = Node(value)
+    if self.has_space():
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+      print(f"Adding {item.get_value()} to the stack!")
+    else:
+      print(f"No room for {item.get_value()}!")
+
+  def pop(self):
+    if not self.is_empty():
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      print(f"Completed {item_to_remove.get_value()}")
+      return item_to_remove.get_value()
+    print("Stack is empty!")
+
+  def peek(self):
+    if not self.is_empty():
+      return self.top_item.get_value()
+    print("Nothing to see here!")
+
+  def has_space(self):
+    return self.limit > self.size
+
+  def is_empty(self) -> bool:
+    return self.size == 0
+
+# Defining an empty pizza stack
+pizza_stack = Stack(6)
+# Adding pizzas as they are ready until we have no more room
+pizza_stack.push("pizza #1")
+pizza_stack.push("pizza #2")
+pizza_stack.push("pizza #3")
+pizza_stack.push("pizza #4")
+pizza_stack.push("pizza #5")
+pizza_stack.push("pizza #6")
+
+# Outputs empty print statement
+pizza_stack.push("pizza #7")
+
+# Delivering pizzas from the top of the stack down
+print(f"The first pizza to deliver is {pizza_stack.peek()}")
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+
+pizza_stack.pop()
---
+++ python/data_structures/stacks_and_queues/stack_two.py
@@ -0,0 +1,8 @@
+
+
+# create a stack class with a top property, it creates an empty stack when instantiated
+# Define a method called isEmpty that takes no argument, and returns a boolean indicating whether or not the stack is empty.
+# should be aware of a default empty value assigned to top when stack is created
+# define a method called push / which takes any value as an argument and adds a new node with that value to the top of the stack with O(1) time performance
+# Define a method called pop that does not take any argument, removes the node from the top of the stack, and returns the node’s value. Should raise exception when called on empty stack
+# Define a method called peek that does not take an argument and returns the value of the node located on top of the stack, without removing it from the stack. Should raise exception when called on empty stack
