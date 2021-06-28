ef7f6f573095ee1866b62e5450c381c75f5b4e0c
ben-hill33 <benjamin.hill28@gmail.com>
2021-06-28 13:17:33 -0700

    added collision handling to assign(), hash(), and retrieve_keys_value() methods

--- data_structures/hashtable/hash_map_two.py
+++ data_structures/hashtable/hash_map_two.py
@@ -6,7 +6,7 @@ class HashMap:
         self.array_size = array_size
         self.array = [None for item in range(array_size)]
 
-    def hash(self, key):
+    def hash(self, key, count_collisions=0):
         """
         Hashing function takes a key and returns an index into the underlying array.
 
@@ -14,7 +14,7 @@ class HashMap:
         """
         key_bytes = key.encode()
         hash_code = sum(key_bytes)
-        return hash_code
+        return hash_code + count_collisions
 
     def compressor(self, hash_code):
         """
@@ -35,6 +35,21 @@ class HashMap:
         if current_val[0] == key:
             self.array[array_index] = [key, value]
             return
+
+        number_collisions = 1
+
+        while current_val[0] != key:
+            new_hash_code = self.hash(key, number_collisions)
+            new_array_index = self.compressor(new_hash_code)
+            current_val = self.array[new_array_index]
+
+            if current_val is None:
+                self.array[array_index] = [key, value]
+                return
+            if current_val[0] == key:
+                self.array[array_index] = [key, value]
+                return
+            number_collisions += 1
         return
 
     def retrieve_keys_value(self, key):
@@ -42,7 +57,28 @@ class HashMap:
         Returns the value of a given key.
         """
         array_index = self.compressor(self.hash(key))
-        return self.array[array_index]
+        possible_value = self.array[array_index]
+
+        if possible_value is None:
+            return None
+
+        if possible_value[0] == key:
+            return possible_value[1]
+        
+        retrieval_collisions = 1
+        
+        while possible_value[0] != key:
+            new_hash_code = self.hash(key, retrieval_collisions)
+            retrieve_index = self.compressor(new_hash_code)
+            possible_value = self.array[retrieve_index]
+
+            if possible_value is None:
+                return None
+            if possible_value[0] == key:
+                return possible_value[1]
+            
+            retrieval_collisions += 1
+        return
 
 hash_map = HashMap(20)
 hash_map.add_key_value("Name", "Ben")
